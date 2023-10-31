4463b0932112cd9fc9eb8cbeba54c36485b70953
NastiaCandor <andubova98@gmail.com>
2023-10-31 21:19:27 +0300

    feat: add catalog burger menu

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -2,18 +2,39 @@ const hamburger = document.querySelector('.hamburger');
 const burgerMenu = document.querySelector('.header__burger-menu');
 const hamburgerClose = document.querySelector('.header__burger-close');
 
-const closeBurgerMenu = () => {
+const catalogLink = document.querySelector('.burger-catalog-link');
+const catalogBurgerMenu = document.querySelector('.header__burger-catalog');
+const catalogBackBtn = document.querySelector('.burger-catalog__back');
+const catalogCloseBtn = document.querySelector('.header__burger-catalog-close');
+
+function closeBurgerMenu() {
   burgerMenu.classList.remove('active');
 };
 
-const openBurgerMenu = () => {
+function openBurgerMenu() {
   burgerMenu.classList.add('active');
 };
 
-hamburger.addEventListener('click', ()=> {
-  openBurgerMenu();
+function closeCatalogBurgerMenu() {
+  catalogBurgerMenu.classList.remove('active');
+};
+
+function openCatalogBurgerMenu () {
+  catalogBurgerMenu.classList.add('active');
+};
+
+hamburger.addEventListener('click', openBurgerMenu);
+
+hamburgerClose.addEventListener('click', closeBurgerMenu);
+
+catalogLink.addEventListener('click', openCatalogBurgerMenu);
+
+console.log(catalogBackBtn);
+catalogBackBtn.addEventListener('click', () => {
+  closeCatalogBurgerMenu();
 });
 
-hamburgerClose.addEventListener('click', () => {
+catalogCloseBtn.addEventListener('click', () => {
+  closeCatalogBurgerMenu();
   closeBurgerMenu();
 });
