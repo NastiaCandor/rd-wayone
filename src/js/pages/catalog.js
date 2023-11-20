9f9f3cb064d4006e19fb701df5f3a6e76010e24d
NastiaCandor <andubova98@gmail.com>
2023-11-20 13:45:09 +0300

    feat: add new catalog detail mobile accord

--- src/js/pages/catalog.js
+++ src/js/pages/catalog.js
@@ -19,3 +19,30 @@ if (document.querySelector('.catalog-banner__swiper')) {
     },
   });
 }
+
+// CATALOG DETAIL
+
+// catalog detail tab change
+if (document.querySelector('.category-catalog__type-wrapper')) {
+  const categoryList = document.querySelectorAll('.category-catalog__type');
+  categoryList.forEach((item) => {
+    item.addEventListener('click',  () => {
+      refreshCatalogList();
+      item.classList.add('active');
+    });
+  });
+  function refreshCatalogList() {
+    categoryList.forEach((el) => {
+      el.classList.remove('active');
+    });
+  }
+}
+
+// benefits items
+const benefitsItems = document.querySelectorAll('.category-benefits__item');
+const benefitsTitleItems = document.querySelectorAll('.category-benefits__title-box');
+benefitsTitleItems.forEach((item, ind) => {
+  item.addEventListener('click', () => {
+    benefitsItems[ind].classList.toggle('active');
+  });
+});
